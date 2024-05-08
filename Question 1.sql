757e62e502aa23a69a784c06a14d7ef075061044
Marko <markogidej@yahoo.com>
2024-05-08 10:33:01 -0400

    Init Commit / Question 1 answered

---
+++ Question 1.sql
@@ -0,0 +1,32 @@
+-- Question 1
+
+USE assignment_sql;
+-- Task 1: SQL Table Creation
+CREATE TABLE Artists (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    formation_year VARCHAR(4) NOT NULL,
+    current_label TEXT
+);
+
+CREATE TABLE Genres (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    greater_category TEXT NOT NULL,
+    specific_category TEXT
+);
+
+CREATE TABLE Albums (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    title TEXT NOT NULL,
+    id_artist INT NOT NULL,
+    id_genre INT NOT NULL,
+    release_year VARCHAR(4) NOT NULL,
+    total_tracks INT NOT NULL,
+    -- Task 3: SQL Foreign Key Establishment
+    FOREIGN KEY (id_artist) REFERENCES Artists(id),
+    FOREIGN KEY (id_genre) REFERENCES Genres(id)
+);
+
+-- Task 2: SQL Alteration
+ALTER TABLE Albums
+ADD duration_min INT NOT NULL;
---
+++ Question 2.sql
@@ -0,0 +1,3 @@
+-- Question 2
+
+-- Task 1: SQL Data Insertion
