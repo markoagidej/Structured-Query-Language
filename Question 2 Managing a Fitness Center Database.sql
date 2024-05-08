50e14657356a1a85684a11731d6ad6854f20a5e6
Marko <markogidej@yahoo.com>
2024-05-08 10:54:14 -0400

    Question 2 answered with assumptions about tables made

--- Question 2 Managing a Fitness Center Database.sql
+++ Question 2 Managing a Fitness Center Database.sql
@@ -1,3 +1,24 @@
 -- Question 2. Managing a Fitness Center Database
 
 -- Task 1: SQL Data Insertion
+INSERT INTO Members
+VALUES
+	('Jane Doe', 25, 1),
+	('John Smitch', 20, 1),
+	('John Smith', 40, 2),
+	('Billy Bob', 17, 2),
+	('Indie Cindy', 29, 3);
+    
+-- Task 2: SQL Data Update
+UPDATE WorkoutSessions
+SET session_time = 'evening'
+WHERE member_name = 'Jane Doe';
+
+-- Task 3: SQL Data Deletion
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM Members
+WHERE name = 'John Smith';
+SET SQL_SAFE_UPDATES = 1;
+-- OR (Assuming the only data in the table is what we just previously inserted)
+DELETE FROM Members
+WHERE id = 3;
