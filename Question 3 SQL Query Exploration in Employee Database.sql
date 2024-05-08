2148e69a84aee2a3a684dd2da55c2f8cdce89dde
Marko <markogidej@yahoo.com>
2024-05-08 11:39:53 -0400

    Question 3 answered

--- Question 3 SQL Query Exploration in Employee Database.sql
+++ Question 3 SQL Query Exploration in Employee Database.sql
@@ -3,9 +3,15 @@
 -- Task 1: SQL DISTINCT Usage
 SELECT DISTINCT d.department_name
 FROM Employees e, Departments d
-WHERE d.department_id  = e.department_id;
+WHERE e.department_id  = d.department_id;
 
 -- Task 2: SQL COUNT Functionality
-
+SELECT d.department_name, COUNT(*)
+FROM Employees e, Departments d
+WHERE e.department_id  = d.department_id
+GROUP BY d.department_id;
 
 -- Task 3: SQL BETWEEN Usage
+SELECT name, age, department_id
+FROM Employees
+WHERE age BETWEEN 25 and 30;
